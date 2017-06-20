7635ecad394ad6b7ada0a2e8a3500e6574080f84
Jeremy Kun <jeremy@21.co>
2017-06-20 14:21:16 -0700

    add 'find closest ancestor' feature

--- main.js
+++ main.js
@@ -87,7 +87,6 @@ function suggest() {
     var results = null;
     if (searchString.length >= 3) {
       results = fuzzyNames.get(searchString, '', 0.3);
-      window.results = results;
     }
 
     let dataList = autocomplete.selectAll('li')
@@ -112,56 +111,149 @@ d3.json("genealogy_graph.json", function(error, d) {
   if (error) throw error;
   console.log('Done loading data');
   data = convertData(d);
+  window.data = data;
   console.log('Done converting data');
   d3.select('#hide_while_loading').style('display', 'block');
   d3.select('#loading').style('display', 'none');
 });
 
 
-function ancestryGraph(id) {
+function shortestPath(graph, source, target) {
+  // DFS to find shortest path
+  // Return a list of edges from source -> target
+  if (source == target) {
+    return [];  // interpreted as truthy
+  }
+
+  if (graph[source]) {
+    for (let child of graph[source]['out']) {
+      let foundEdges = shortestPath(graph, child, target);
+      if (foundEdges) {  // empty list interpreted as truthy
+        foundEdges.unshift([source, child]);  // push new edge to front
+        return foundEdges;
+      }
+    }
+  }
+
+  // returns undefined if at a leaf or target is not found
+  // in any subtree of source
+}
+
+
+function closestAncestor(graph, id1, id2) {
+  // perform a simultaneous BFS from each child node
+  // until an ancestor is found; return undefined if no ancetor found
+  let unprocessed1 = [id1];
+  let unprocessed2 = [id2];
+  let processed1 = new Set([]);
+  let processed2 = new Set([]);
+
+  function handle(unprocessed, myProcessed, otherProcessed) {
+    // one step of the BFS
+    if (unprocessed.length > 0) {
+      let next = unprocessed.shift();
+      if (otherProcessed.has(next)) {
+        return next;
+      }
+      myProcessed.add(next);
+      let parents = graph[next]['in'];
+
+      for (let parentId of parents) {
+        if (otherProcessed.has(parentId)) {
+          return parentId;
+        } else {
+          unprocessed.push(parentId);
+        }
+      }
+    }
+  }
+
+  while (unprocessed1.length > 0 || unprocessed2.length > 0) {
+    // interleave the BFS from each child, stopping if a common parent is found
+    let possibleParent = handle(unprocessed1, processed1, processed2);
+    if (possibleParent) {
+      return possibleParent;  // it's a definite parent
+    }
+
+    possibleParent = handle(unprocessed2, processed2, processed1);
+    if (possibleParent) {
+      return possibleParent;  // it's a definite parent
+    }
+  }
+
+  // no common ancestors!
+}
+
+
+function commonAncestryGraph(graph, id1, id2) {
+  let ancestor = closestAncestor(graph, id1, id2);
+  if (!ancestor) {
+    console.log('No ancestors found for ' + id1 + ' ' + id2);
+    return null;
+  } else {
+    console.log('Closest ancestor is ' + graph[ancestor].name + ' with id ' + ancestor);
+  }
+
+  let edges1 = shortestPath(graph, ancestor, id1);
+  let edges2 = shortestPath(graph, ancestor, id2);
+  let union = [];
+  for (let x of edges1) {
+    union.push(x);
+  }
+  for (let x of edges2) {
+    union.push(x);
+  }
+
+  return union;
+}
+
+
+function ancestryGraph(graph, id, parentsOnly=false) {
   // Construct the graph of ancestors of the given node
-  // Output is a d3-compatible edge list
-  let nodeIdToName = {};
-  let edgeSubset = [];
+  // Output is a Set of edges
+  let edgeSubset = new Set([]);
   let unprocessedAncestors = [id];
   let processed = new Set([]);
 
   while (unprocessedAncestors.length > 0) {
     let next = unprocessedAncestors.pop();
     processed.add(next);  // Ignore any self loops, which would be odd
-    nodeIdToName[next] = data[next].name;
-    let parents = data[next]['in'];
+    let parents = graph[next]['in'];
 
     for (let parentId of parents) {
-      edgeSubset.push([parentId, next]);
+      edgeSubset.add([parentId, next]);
       if (!processed.has(parentId)) {
         unprocessedAncestors.push(parentId);
       }
     }
   }
 
-  let unprocessedDescendants = [id];
-  while (unprocessedDescendants.length > 0) {
-    let next = unprocessedDescendants.pop();
-    processed.add(next);  // Ignore any self loops, which would be odd
-    nodeIdToName[next] = data[next].name;
-    let children = data[next]['out'];
+  if (!parentsOnly) {
+    let unprocessedDescendants = [id];
+    while (unprocessedDescendants.length > 0) {
+      let next = unprocessedDescendants.pop();
+      processed.add(next);  // Ignore any self loops, which would be odd
+      let children = graph[next]['out'];
 
-    for (let childId of children) {
-      edgeSubset.push([next, childId]);
-      if (!processed.has(childId)) {
-        unprocessedDescendants.push(childId);
+      for (let childId of children) {
+        edgeSubset.add([next, childId]);
+        if (!processed.has(childId)) {
+          unprocessedDescendants.push(childId);
+        }
       }
     }
   }
 
-  let edges = edgeSubset.map(function(e) {
-    let sourceName = nodeIdToName[e[0].toString()];
-    let targetName = nodeIdToName[e[1].toString()];
+  return edgeSubset;
+}
+
+
+function edgeListToStrings(edges) {
+  return edges.map(function(e) {
+    let sourceName = data[e[0]].name;
+    let targetName = data[e[1]].name;
     return '"' + sourceName + '" -> "' + targetName + '";';
   });
-
-  return edges;
 }
 
 
@@ -169,8 +261,10 @@ function ancestryGraph(id) {
 var render = dagreD3.render();
 
 function createGraphFor(id) {
-  var graph;
-  let edgeStrings = ancestryGraph(id);
+  let graph;
+  let gauss = 18231;
+  let jeremy = 203505;
+  let edgeStrings = edgeListToStrings(commonAncestryGraph(data, id, jeremy));
   let graphString = "digraph { ";
 
   graphString = graphString + " \"" + data[id].name + "\" [style=\"fill: #66ff66; font-weight: bold\"];";
@@ -205,7 +299,6 @@ function createGraphFor(id) {
 
 function renderFromSearch() {
   let name = d3.select('#name_input').property("value");
-  console.log('Looking up ' + name);
   name = name.trim();
 
   let found_id = null;
@@ -231,3 +324,7 @@ d3.select('#ancestry_button').on('click', renderFromSearch);
 d3.select('#autocomplete_results').style('display', 'none');
 d3.select('#loading').style('display', 'block');
 d3.select('#hide_while_loading').style('display', 'none');
+
+
+window.shortestPath = shortestPath;
+window.closestAncestor = closestAncestor;
